4b6e4e30b9e69f09f31356cde17e840b4f99eb83
Trisakti Akbar <trisaktiakbar01@gmail.com>
2023-08-10 21:05:11 +0800

    sss

--- footer/footer.php
+++ footer/footer.php
@@ -3,7 +3,7 @@
         <div class="d-flex align-items-center justify-content-between small">
             <div class="text-muted" id="footer-copyright">2022 &copy; Polres Mamuju Tengah</div>
             <div>
-                <p class="mb-0 pb-0 text-muted" id="footer-text">Siste Informasi Manajemen Pengaduan Tindak Pidana dan Pelanggaran (SIMPANAN)</p>
+                <p class="mb-0 pb-0 text-muted" id="footer-text">Sistem Informasi Manajemen Pengaduan Tindak Pidana dan Pelanggaran (SIMPANAN)</p>
             </div>
         </div>
     </div>
